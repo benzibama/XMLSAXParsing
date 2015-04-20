f3397a7ee14b16b7efb412e46d15759ff0137170
Benzibama Sabapathi <benzibama.s@gmail.com>
2015-04-20 06:46:12 -0700

    1 commit

---
+++ XMLSAXParsing/app/src/main/java/com/json/benzibama/android/xmlsaxparsing/PostValue.java
@@ -0,0 +1,32 @@
+package com.json.benzibama.android.xmlsaxparsing;
+
+/**
+ * Created by bama on 4/17/2015.
+ */
+public class PostValue {
+    String guid,title,date;
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
